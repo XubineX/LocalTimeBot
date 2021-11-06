ce3fcfd8ac73e08aedb05929686c37ecae6dcd79
jayKay <kubus729@gmail.com>
2021-11-06 16:36:43 +0000

    transfer to command handling folder

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('fs');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.once('ready', () => {
 	console.log('bot dziala yupi');
 });
 
+for (const file of commandFiles) {
+	const command = require('./commands/' + file);
+	client.commands.set(command.data.name, command);
+}
+
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 	
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 	
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server name: '+ interaction.guild.name + '\nTotal members: ' + interaction.guild.memberCount);
-	} else if (commandName === 'user') {
-		await interaction.reply('User info: ' + interaction.user.tag + '\nYour id: ' + interaction.user.id);
+	if (!command) return;
+	
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
 	}
 });
 
