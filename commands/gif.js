6435880eaf18b1bd00c4970aec310a6971ca9877
jayKay <kubus729@gmail.com>
2021-11-07 10:49:56 +0000

    added auto delete to gif

--- commands/gif.js
+++ commands/gif.js
@@ -16,8 +16,12 @@ module.exports = {
 		const category = interaction.options.getString('category');
 		if (category === 'gif_funny') {
 			await interaction.reply('https://giphy.com/clips/theblackphone-the-black-phone-movie-EKno5KuNoHfgBHB23L');
+			await wait(2000);
+			await interaction.deleteReply();
 		} else if (category === 'gif_meme') {
 			await interaction.reply('https://i.pinimg.com/736x/5d/0f/0e/5d0f0ea84d49766b304326cfdb93d1d6.jpg');
+			await wait(2000);
+			await interaction.deleteReply();
 		}
 	},
 };
